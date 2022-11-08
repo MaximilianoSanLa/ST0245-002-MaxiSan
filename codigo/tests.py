6a8ec76b4b254bd49eac2cb8532a673b414c070d
MaximilianoSanLa <104327901+MaximilianoSanLa@users.noreply.github.com>
2022-11-08 00:05:36 -0500

    Delete tests.py

--- codigo/tests.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-poligono = pd.read_csv('https://raw.githubusercontent.com/mauriciotoro/ST0245-Eafit/master/proyecto/Datasets/poligono_de_medellin.csv', sep=';')
-print(poligono)
-acoso = pd.read_csv('https://raw.githubusercontent.com/mauriciotoro/ST0245-Eafit/master/proyecto/Datasets/calles_de_medellin_con_acoso.csv', sep=';')
-print(acoso)
